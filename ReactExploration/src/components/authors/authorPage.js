749dbb33526cf67549df102e753fe548cae3b031
Mike Rousos <Mike@MJRousos.com>
2017-04-21 17:14:58 -0400

    Pass data from a controller view to a child component with props

--- ReactExploration/src/components/authors/authorPage.js
+++ ReactExploration/src/components/authors/authorPage.js
@@ -2,46 +2,33 @@
 
 var React = require('React');
 var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
 
 class Authors extends React.Component {
   constructor() {
     super();
+
+    this.state = { authors: [] };
   }
 
-  // Initialize the shape of our state
-  getInitialState() {
-    return {
-      authors: []
-    };
-  }
+  // Not used in ES6 - instead, just set this.state in the ctor
+  // // Initialize the shape of our state
+  // getInitialState() {
+  //   return {
+  //     authors: []
+  //   };
+  // }
 
   // Populate state prior to loading the component
-  componentWillMount() {
+  componentDidMount() {
     this.setState({ authors: AuthorApi.getAllAuthors() });
   }
 
   render() {
-    var createAuthorRow = function (author) {
-      return (
-        <tr key={author.id}>
-          <td><a href={"/#authors/" + author.id}>{author.id}</a></td>
-          <td>{author.firstName} {author.lastName}</td>
-        </tr>
-      );
-    };
-
     return (
       <div>
         <h1>Authors</h1>
-        <table className="table">
-          <thead>
-            <th>ID</th>
-            <th>Name</th>
-          </thead>
-          <tbody>
-            {this.state.authors.map(createAuthorRow, this)}
-          </tbody>
-        </table>
+        <AuthorList authors={this.state.authors} />
       </div>
     );
   }
